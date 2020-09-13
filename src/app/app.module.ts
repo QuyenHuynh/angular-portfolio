163c2f69d0abb4d04295f1c6ce34b89173469b39
Quyen Huynh <quyenh@hotmail.com>
2020-09-13 01:43:46 -0400

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { ResumeComponent } from './component/resume/resume.component';
+import { HomeComponent } from './component/home/home.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { PortfolioComponent } from './component/portfolio/portfolio.component';
+import { HeaderComponent } from './component/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    ResumeComponent,
+    HomeComponent,
+    ContactComponent,
+    PortfolioComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
